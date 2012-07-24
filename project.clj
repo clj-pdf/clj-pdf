b5d14f45bc068a43c96ec2d644e94c210a6fd082
Yogthos <dmitri.sotnikov@gmail.com>
2012-07-24 15:54:07 -0300

    updated project file

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject clj-pdf 
   "0.9.7-SNAPSHOT"
   :description "PDF generation library"
+  :license {:name "GNU Lesser General Public License - v 3"
+            :url "http://www.gnu.org/licenses/lgpl.html"
+            :distribution :repo
+            :comments "same as  iText version 2.1.7 and JFreeChart"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [jfree/jfreechart "1.0.13"]
                  [com.lowagie/itext "2.1.7"]])
