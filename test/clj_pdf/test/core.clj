6eff72db7189b53dc046b9ffbf5c998cca156027
Richard Hull <rm_hull@yahoo.co.uk>
2013-09-24 08:57:28 +0100

    Remove surplus whitespace

--- test/clj_pdf/test/core.clj
+++ test/clj_pdf/test/core.clj
@@ -3,7 +3,7 @@
   (:require [clojure.string :as re]))
 
 (defn doc-to-str [doc]
-  (let [out (new java.io.ByteArrayOutputStream)] 
+  (let [out (new java.io.ByteArrayOutputStream)]
     (write-doc doc out)
   (.toString out)))
 
@@ -13,9 +13,9 @@
   (re/replace #"CreationDate\((.*?)\)" "")
   (re/replace #"\[(.*?)\]" "")))
 
-(defn eq? [doc1 doc2]  
+(defn eq? [doc1 doc2]
   ;uncomment to generate test data
-  ;(spit (str "test" java.io.File/separator doc2) (fix-pdf (doc-to-str doc1)))  
+  ;(spit (str "test" java.io.File/separator doc2) (fix-pdf (doc-to-str doc1)))
   (is (= (fix-pdf (doc-to-str doc1)) (fix-pdf (slurp (str "test" java.io.File/separator doc2))))))
 
 (deftest page-numbers
@@ -26,7 +26,7 @@
       :top-margin    20
       :bottom-margin 25
       :pages true
-      :font  {:size 11}  
+      :font  {:size 11}
       :size          :a4
       :orientation   "landscape"
       :subject "Some subject"
@@ -38,7 +38,7 @@
      [:paragraph "I should have font size 11"]
      [:chunk "meta test"]]
     "pages1.pdf")
-  
+
   (eq?
     [{:title  "Test doc"
       :left-margin   10
@@ -46,8 +46,8 @@
       :top-margin    20
       :bottom-margin 25
       :pages true
-      :font  {:size 11}  
-      :size          :a4      
+      :font  {:size 11}
+      :size          :a4
       :subject "Some subject"
       :author "John Doe"
       :creator "Jane Doe"
@@ -60,32 +60,32 @@
     "pages2.pdf"))
 
 (deftest image
-  (eq? 
-    [{}      
-     [:image 
+  (eq?
+    [{}
+     [:image
       {:xscale     0.5
-       :yscale     0.8       
+       :yscale     0.8
        :align      :center
        :annotation ["FOO" "BAR"]
        :pad-left   100
        :pad-right  50}
       (javax.imageio.ImageIO/read (new java.io.File (str "test" java.io.File/separator "mandelbrot.jpg")))]]
     "image.pdf")
-  (eq? 
-    [{}      
-     [:image 
-      {:scale     12              
+  (eq?
+    [{}
+     [:image
+      {:scale     12
        :align      :center
        :annotation ["FOO" "BAR"]
        :pad-left   100
        :pad-right  50}
       (javax.imageio.ImageIO/read (new java.io.File (str "test" java.io.File/separator "mandelbrot.jpg")))]]
     "image1.pdf")
-  (eq? 
-    [{}      
-     [:image 
+  (eq?
+    [{}
+     [:image
       {:width     30
-       :height     50       
+       :height     50
        :align      :center
        :annotation ["FOO" "BAR"]}
       (javax.imageio.ImageIO/read (new java.io.File (str "test" java.io.File/separator "mandelbrot.jpg")))]]
@@ -98,7 +98,7 @@
       :right-margin  50
       :top-margin    20
       :bottom-margin 25
-      :font  {:size 11}  
+      :font  {:size 11}
       :size          :a4
       :orientation   "landscape"
       :subject "Some subject"
@@ -111,23 +111,23 @@
      [:chunk "meta test"]]
     "header.pdf"))
 
-(deftest table 
-  (eq? 
+(deftest table
+  (eq?
     [{}
      [:table {:header ["Row 1" "Row 2" "Row 3"] :width 50 :border false :cell-border false}
-      [[:cell {:colspan 2} "Foo"] "Bar"]             
-      ["foo1" "bar1" "baz1"] 
+      [[:cell {:colspan 2} "Foo"] "Bar"]
+      ["foo1" "bar1" "baz1"]
       ["foo2" "bar2" "baz2"]]
-     
+
      [:table {:border-width 10 :header ["Row 1" "Row 2" "Row 3"]} ["foo" "bar" "baz"] ["foo1" "bar1" "baz1"] ["foo2" "bar2" "baz2"]]
-     
+
      [:table {:border false :header [{:color [100 100 100]} "Singe Header"]} ["foo" "bar" "baz"] ["foo1" "bar1" "baz1"] ["foo2" "bar2" "baz2"]]
-     
-     [:table {:cell-border false :header [{:color [100 100 100]} "Row 1" "Row 2" "Row 3"] :cellSpacing 20 :header-color [100 100 100]} 
-      ["foo" 
-       [:cell [:phrase {:style :italic :size 18 :family :halvetica :color [200 55 221]} "Hello Clojure!"]] 
-       "baz"] 
-      ["foo1" [:cell {:color [100 10 200]} "bar1"] "baz1"] 
+
+     [:table {:cell-border false :header [{:color [100 100 100]} "Row 1" "Row 2" "Row 3"] :cellSpacing 20 :header-color [100 100 100]}
+      ["foo"
+       [:cell [:phrase {:style :italic :size 18 :family :halvetica :color [200 55 221]} "Hello Clojure!"]]
+       "baz"]
+      ["foo1" [:cell {:color [100 10 200]} "bar1"] "baz1"]
       ["foo2" "bar2" "baz2"]]]
     "table.pdf"))
 
@@ -140,7 +140,7 @@
        "chapter.pdf"))
 
 (deftest anchor
-  (eq? [{} 
+  (eq? [{}
         [:anchor {:style {:size 15} :leading 20} "some anchor"]
         [:anchor [:phrase {:style :bold} "some anchor phrase"]]
         [:anchor "plain anchor"]
@@ -155,7 +155,7 @@
 
 
 (deftest phrase
-  (eq? [{} 
+  (eq? [{}
        [:phrase "some text here"]
        [:phrase {:style :italic :size 18 :family :halvetica :color [0 255 221]} "Hello Clojure!"]
        [:phrase [:chunk {:style :strikethru} "chunk one"] [:chunk {:size 20} "Big text"] "some other text"]]
@@ -168,7 +168,7 @@
        [:paragraph {:keep-together true :indent 20} "a fine paragraph"]
        [:paragraph {:align :center} "centered paragraph"]
        [:paragraph "256" [:chunk {:super true} "5"] " and 128" [:chunk {:sub true} "2"]]]
-       
+
       "paragraph.pdf"))
 
 (deftest list-test
@@ -178,27 +178,27 @@
 #_(deftest chart
   (eq? [{}
        [:chart {:type :bar-chart :title "Bar Chart" :x-label "Items" :y-label "Quality"} [2 "Foo"] [4 "Bar"] [10 "Baz"]]
-       [:chart {:type :line-chart :title "Line Chart" :x-label "checkpoints" :y-label "units"} 
+       [:chart {:type :line-chart :title "Line Chart" :x-label "checkpoints" :y-label "units"}
              ["Foo" [1 10] [2 13] [3 120] [4 455] [5 300] [6 600]]
              ["Bar" [1 13] [2 33] [3 320] [4 155] [5 200] [6 300]]]
        [:chart {:type :pie-chart :title "Big Pie"} ["One" 21] ["Two" 23] ["Three" 345]]
        [:chart {:type :line-chart :time-series true :title "Time Chart" :x-label "time" :y-label "progress"}
              ["Incidents"
-              ["2011-01-03-11:20:11" 200] 
-              ["2011-01-03-11:25:11" 400] 
-              ["2011-01-03-11:35:11" 350] 
+              ["2011-01-03-11:20:11" 200]
+              ["2011-01-03-11:25:11" 400]
+              ["2011-01-03-11:35:11" 350]
               ["2011-01-03-12:20:11" 600]]]
-       [:chart {:type :line-chart 
-                     :time-series true 
+       [:chart {:type :line-chart
+                     :time-series true
                      :time-format "MM/yy"
-                     :title "Time Chart" 
-                     :x-label "time" 
+                     :title "Time Chart"
+                     :x-label "time"
                      :y-label "progress"}
              ["Occurances" ["01/11" 200] ["02/12" 400] ["05/12" 350] ["11/13" 600]]]]
       "charts.pdf"))
 
 (deftest heading
-  (eq? [{} [:heading "Lorem Ipsum"]    
+  (eq? [{} [:heading "Lorem Ipsum"]
        [:heading {:size 15} "Lorem Ipsum"]
        [:heading {:align :center} "Centered"]]
       "heading.pdf"))
@@ -225,7 +225,7 @@
         [:section "Section Title"
          [:paragraph "Some content"]
          [:paragraph "Some more content"]
-         [:section [:paragraph "Nested Section Title"]               
+         [:section [:paragraph "Nested Section Title"]
           [:paragraph "nested section content"]]]]]
       "section.pdf"))
 
