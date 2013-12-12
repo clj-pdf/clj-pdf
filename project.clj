9592d6b14d2dada01cd6dc3e960dc2f1ba00b307
Yogthos <dmitri.sotnikov@gmail.com>
2013-12-12 09:12:17 -0500

    bumped up version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-pdf
-  "1.11.6"
+  "1.11.7"
   :description "PDF generation library"
   :url "https://github.com/yogthos/clj-pdf"
   :license {:name "GNU Lesser General Public License - v 3"
