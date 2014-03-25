fb0f00d6caf46076e0e5f10652843173123d6d57
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2014-03-25 13:27:03 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
             :url "http://www.gnu.org/licenses/lgpl.html"
             :distribution :repo
             :comments "same as  iText and JFreeChart"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.jfree/jfreechart "1.0.15"]
                  [com.lowagie/itext "4.2.1"]
                  [org.apache.xmlgraphics/batik-gvt "1.7"]])
