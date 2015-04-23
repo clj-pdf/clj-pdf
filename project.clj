8389807e4722ae14dc98b8a255d6b3b47e2e0571
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-04-23 17:00:50 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-pdf "2.0.2"
+(defproject clj-pdf "2.0.3"
   :description "PDF generation library"
   :url "https://github.com/yogthos/clj-pdf"
   :license {:name "GNU Lesser General Public License - v 3"
