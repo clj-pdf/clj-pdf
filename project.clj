0fd0491aaa162fdcf248284e337e22527c8d18c9
Timothy Roy <roy.tim@gmail.com>
2015-07-12 16:43:47 -0500

    Updated dependency in project.clj, and :require in graphics_2d.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-pdf "2.0.6"
+(defproject org.clojars.coyotespike/clj-pdf "2.0.6"
   :description "PDF generation library"
   :url "https://github.com/yogthos/clj-pdf"
   :license {:name "GNU Lesser General Public License - v 3"
@@ -7,13 +7,15 @@
             :comments "same as  iText and JFreeChart"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.jfree/jfreechart "1.0.15"]
-                 [com.lowagie/itext "4.2.1"]
+;                 [com.lowagie/itext "4.2.1"]
+                 [com.itextpdf/itextpdf "5.0.6"]
                  [org.apache.xmlgraphics/batik-gvt "1.7"]]
 
   :plugins [[lein-marginalia "0.7.1"]
             [lein-midje "3.0.0"]
             [midje-readme "1.0.2"]]
 
+  :signing {:gpg-key "timothy.roy@protonmail.ch"}
   :profiles {:dev {:global-vars {*warn-on-reflection* true}
                    :dependencies [[midje "1.5.0"]
                                   [environ "0.5.0"]
