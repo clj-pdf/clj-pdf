31c50ad5b57663601c768e461b61f0afa5363a96
Yogthos <dmitri.sotnikov@gmail.com>
2015-07-14 11:09:42 -0400

    bumped up project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-pdf "2.0.8"
+(defproject clj-pdf "2.0.9"
   :description "PDF generation library"
   :url "https://github.com/yogthos/clj-pdf"
   :license {:name "GNU Lesser General Public License - v 3"
@@ -7,7 +7,7 @@
             :comments "same as  iText and JFreeChart"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.jfree/jfreechart "1.0.15"]
-                 [itext "4.2.0"]
+                 [itext "4.2.1"]
                  [org.apache.xmlgraphics/batik-gvt "1.7"]]
 
   :plugins [[lein-marginalia "0.7.1"]
@@ -15,6 +15,6 @@
             [midje-readme "1.0.2"]]
 
   :profiles {:dev {:global-vars {*warn-on-reflection* true}
-                   :dependencies [[midje "1.5.0"]
+                   :dependencies [[midje "1.7.0"]
                                   [environ "0.5.0"]
-                                  [midje-readme "1.0.2"]]}})
+                                  [midje-readme "1.0.8"]]}})
