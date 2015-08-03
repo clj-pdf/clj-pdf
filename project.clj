b9ed6dfa50db03568afaa47e135b27e13d721fb9
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-08-03 14:51:20 -0400

    removed dependencies present in itext

--- project.clj
+++ project.clj
@@ -6,9 +6,7 @@
             :distribution :repo
             :comments "same as  iText and JFreeChart"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.jfree/jfreechart "1.0.15"]
-                 [itext "4.2.1"]
-                 [org.apache.xmlgraphics/batik-gvt "1.7"]]
+                 [itext "4.2.1"]]
 
   :plugins [[lein-marginalia "0.7.1"]
             [lein-midje "3.0.0"]
