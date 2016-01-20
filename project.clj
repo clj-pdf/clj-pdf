13d3f4dbe4da337095001b2a55faf414014c6ff0
Yogthos <dmitri.sotnikov@gmail.com>
2016-01-20 17:27:54 -0500

    bumped up dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-pdf "2.1.8"
+(defproject clj-pdf "2.1.9"
   :description "PDF generation library"
   :url "https://github.com/yogthos/clj-pdf"
 
@@ -8,7 +8,7 @@
 
   :dependencies [[dom4j "1.6.1"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.jfree/jfreechart "1.0.15"]
+                 [org.jfree/jfreechart "1.0.19"]
                  [org.apache.xmlgraphics/batik-gvt "1.7"]
                  [org.swinglabs/pdf-renderer "1.0.5"]]
   :resource-paths ["src/java"]
@@ -17,8 +17,8 @@
   :javac-options ["-target" "1.6" "-source" "1.6"]
 
   :profiles {:dev     {:global-vars  {*warn-on-reflection* true}
-                       :dependencies [[midje "1.7.0"]
-                                      [environ "0.5.0"]
+                       :dependencies [[midje "1.8.3"]
+                                      [environ "1.0.1"]
                                       [midje-readme "1.0.8"]]
                        :plugins      [[lein-marginalia "0.7.1"]
                                       [lein-midje "3.0.0"]
