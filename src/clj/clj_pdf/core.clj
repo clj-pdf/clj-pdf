916a95883d0cf54bac3c3a85cc6d5ace75999a4a
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-07-11 15:50:53 -0400

    added a merge functions

--- src/clj/clj_pdf/core.clj
+++ src/clj/clj_pdf/core.clj
@@ -1018,6 +1018,23 @@
           (seq? in) (seq-to-doc in out)
           :else (write-doc in out))))
 
+(defn merge
+  "usage: takes an output that can be a file name or an output stream followed by one or more documents
+   that can be input streams, urls, filenames, or byte arrays." 
+  [out & pdfs]
+  (let [doc (Document.)
+        wrt (PdfWriter/getInstance doc out)
+        cb (do (.open doc) (.getDirectContent wrt))]
+    (doseq [pdf pdfs]
+      (let [rdr (PdfReader. pdf)]
+        (dotimes [i (.getNumberOfPages rdr)]
+          (.newPage doc)
+          (.addTemplate cb (.getImportedPage wrt rdr (inc i)) 0 0))
+        (.close rdr)))
+    (.flush out)
+    (.close doc)
+    (.close out)))
+  
 ;;;templating
 (defmacro template [t]
   `(fn [~'items]
