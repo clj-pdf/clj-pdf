c626d3a9382bf01695034491d3854c3a9c6adda7
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2023-08-28 09:28:35 -0400

    add missing test helper functions back

--- test/clj_pdf/test/core.clj
+++ test/clj_pdf/test/core.clj
@@ -3,6 +3,86 @@
   (:require [clj-pdf.utils :as utils]
             [clojure.string :as re]))
 
+
+(defn add-test-path-prefix [filename]
+  (str "test" java.io.File/separator filename))
+
+(defn fix-pdf [^bytes pdf-bytes]
+  (-> (String. pdf-bytes)
+      ; obviously these will get changed on each run, so strip the creation/modification date/times
+      (re/replace #"ModDate\((.*?)\)" "")
+      (re/replace #"CreationDate\((.*?)\)" "")
+      ; not sure what this is for ... ?
+      (re/replace #"\[(.*?)\]" "")
+      ; these are kind of hacky, but it seems that the prefix characters before the font name "Carlito"
+      ; will get randomly generated on each run ... ?
+      (re/replace #"Font\/([A-Z]+\+Carlito)" "Font/PHZPHX+Carlito")
+      (re/replace #"FontBBox\/([A-Z]+\+Carlito)" "FontBBox/PHZPHX+Carlito")
+      (re/replace #"FontName\/([A-Z]+\+Carlito)" "FontName/PHZPHX+Carlito")
+      (re/replace #"BaseFont\/([A-Z]+\+Carlito)" "BaseFont/PHZPHX+Carlito")
+
+      (re/replace #"Font\/([A-Z]+\+CourierPrime)"
+                  "Font/BUKDVX+CourierPrime")
+      (re/replace #"FontBBox\/([A-Z]+\+CourierPrime)"
+                  "FontBBox/BUKDVX+CourierPrime")
+      (re/replace #"FontName\/([A-Z]+\+CourierPrime)"
+                  "FontName/BUKDVX+CourierPrime")
+      (re/replace #"BaseFont\/([A-Z]+\+CourierPrime)"
+                  "BaseFont/BUKDVX+CourierPrime")))
+
+(defn read-file ^bytes [file-path]
+  (with-open [reader (input-stream file-path)]
+    (let [length (.length (file file-path))
+          buffer (byte-array length)]
+      (.read reader buffer 0 length)
+      buffer)))
+
+(defn pdf->bytes ^bytes [doc]
+  (let [out (new java.io.ByteArrayOutputStream)]
+    (pdf doc out)
+    (.toByteArray out)))
+
+(defn inject-test-font [doc]
+  (let [font-filename (add-test-path-prefix "Carlito-Regular.ttf")
+        font-props    {:encoding :unicode
+                       :ttf-name font-filename}]
+    (update-in (vec doc) [0 :font] merge font-props)))
+
+(defn generate-pdf [doc output-filename & _]
+  (let [doc1 (inject-test-font doc)]
+    (println "regenerating pdf" output-filename)
+    (pdf doc1 (add-test-path-prefix output-filename))
+    true))
+
+(defn doc->stream [doc]
+  (let [s (new java.io.ByteArrayOutputStream)]
+    (binding [*out* (writer s)]
+      (doseq [x doc]
+        (pr x))
+      (flush))
+    (input-stream (.toByteArray s))))
+
+(defn eq? [doc1 filename & {:keys [stream] :or {stream true}}]
+  (let [filename   (add-test-path-prefix filename)
+        doc1       (inject-test-font doc1)
+        doc1-bytes (pdf->bytes doc1)
+        seq-doc1-bytes (pdf->bytes (seq doc1))
+        stream-doc1-bytes (when stream (pdf->bytes (doc->stream doc1)))
+        doc2-bytes (read-file filename)]
+    (is (= (fix-pdf doc1-bytes)
+           (fix-pdf doc2-bytes)))
+    (is (= (fix-pdf seq-doc1-bytes)
+           (fix-pdf doc2-bytes))
+        "seq")
+    (when stream
+      (is (= (fix-pdf stream-doc1-bytes)
+             (fix-pdf doc2-bytes))
+          "stream"))))
+
+(defn regenerate-test-pdfs []
+  (with-redefs [eq? generate-pdf]
+    (run-tests)))
+
 ; run this to regenerate all test pdfs
 #_(regenerate-test-pdfs)
 
